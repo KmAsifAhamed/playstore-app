a97de4d3dce028d8cf2444a8c08c0c40a607e49e
KmAsufAhamed <asif345ctg@gmail.com>
2023-09-12 17:45:14 +0600

    apps commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function alonewolf()
+{
+  alert("The real Play Store opens.")
+  open("https://play.google.com/store/apps/dev?id=9039357552128419825")
+}
+
+function install()
+{
+  alert("The real Play Store opens.")
+  open("https://play.google.com/store/apps/details?id=com.alonewolf.codeflash")
+}
